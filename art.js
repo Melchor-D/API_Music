194c6a15dbc42adebb88f7f950ce38d7bc72c4cc
Melchor-D <melchorfrisly8@gmail.com>
2026-05-06 18:49:35 -0600

    feat:Animacion

--- art.js
+++ art.js
@@ -1,18 +1,1 @@
-fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects')
-.then(response => response.json())
-.then(data => console.log(data));
-
-    ids.forEach(id => {
-      fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`)
-        .then(res => res.json())
-        .then(obj => {
-          console.log(obj);
-
-          const div = document.createElement('div');
-          div.innerHTML = `
-            <h2>${obj.title}</h2>
-            <img src="${obj.primaryImageSmall}" width="200"/>
-          `;
-          document.body.appendChild(div);
-        });
-    });
+fetch("https://api.deezer.com/search?q=eminem")
